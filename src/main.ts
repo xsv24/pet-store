6c8a6d26f3f8afccb6db80e5b445c3fd2cc03751
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-06-22 21:48:01 +1200

    Add swagger setup.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pet store')
+    .setDescription('Pet store API spec')
+    .setVersion('1.0')
+    .addTag('pets')
+    .build();
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config))
+
   await app.listen(3000);
 }
 bootstrap();
