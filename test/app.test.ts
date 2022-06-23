8e9ed50604000a7e5cbd8ef54e4f2d3f4022fc73
Thomas Pearson <thomaspearson.dev@gmail.com>
2022-06-23 18:08:50 +1200

    Fix up end to end test.

--- test/app.test.ts
+++ test/app.test.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/pets (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/v1/pets')
       .expect(200)
-      .expect('Hello World!');
+      .expect([]);
   });
 });
